f56619b0a4e8dd9966949d926f8622cde9a6c016
RavinduNBA <57413176+RavinduNBA@users.noreply.github.com>
2025-01-25 22:10:30 +0530

    Dashboard added

--- app/src/main/java/com/sasmi/controlit/DashboardActivity.java
+++ app/src/main/java/com/sasmi/controlit/DashboardActivity.java
@@ -4,21 +4,58 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.sasmi.controlit.databinding.ActivityDashboardBinding;
+import com.sasmi.controlit.fragments.*;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    ActivityDashboardBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.add_item) {
+                replaceFragment(new AddItemFragment());
+            } else if (itemId == R.id.notifications) {
+                replaceFragment(new AlertsFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new AccountFragment());
+            }
+
+            return true;
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
+
     }
 }
